577f54858d7d9160bf7f8146a07dd73b3d3e166e
Danil Turiv <turivdaniil@gmail.com>
2026-04-27 02:23:40 +0200

    feat(footer): add responsive layout and email validation

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -18,3 +18,42 @@ servicesGridButtons.forEach((item) => {
         info.classList.toggle('is-open')
     })    
 })
+
+const footerForm = document.querySelector('.footer__newsletter-form')
+const footerInput = document.querySelector('#footer-input')
+const footerButton = document.querySelector('#footer-button')
+const footerMessage = document.querySelector('.footer__newsletter-message')
+const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const footerEmailMessage = {
+    empty: 'Please enter your email address.',
+    error: 'Please enter a valid email address.',
+    valid: 'Thank you! Your email address is valid.'
+}
+
+const setMessage = (message, isValid) => {
+    footerInput.classList.toggle('valid', isValid)
+    footerInput.classList.toggle('error', !isValid)
+    footerMessage.classList.toggle('valid', isValid)
+    footerMessage.classList.toggle('error', !isValid)
+    footerMessage.textContent = message
+}
+
+footerButton.addEventListener('click', () => {
+    const footerEmailValue = footerInput.value
+
+    if (!footerEmailValue) {
+        setMessage(footerEmailMessage.empty, false)
+    }
+
+    else if (!regex.test(footerEmailValue)) {
+        setMessage(footerEmailMessage.error, false)
+    }
+
+    else {
+        setMessage(footerEmailMessage.valid, true)
+        setTimeout(() => {
+            footerForm.submit()      
+        }, 1000)
+    }
+})
